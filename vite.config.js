6a47a83ed01e6964b67cf3f4e0a22165e65e19f5
l1t48 <adnanhmndan7@gmail.com>
2025-09-22 16:02:45 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: "/Portfolio",
+  base: "/Portfolio/",
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     tailwindcss(),
   ],
